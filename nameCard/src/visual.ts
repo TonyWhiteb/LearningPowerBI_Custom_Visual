f5c9eabeed702988478f4cbe0378ef089d625b29
TonyWhite <tonybh2016@gmail.com>
2017-07-10 15:54:24 -0500

    'NameCard'

---
+++ nameCard/src/visual.ts
@@ -0,0 +1,28 @@
+
+module powerbi.extensibility.visual {
+    export interface ViewModel {
+        value: number
+    }
+    export class Visual implements IVisual {
+        private svg:
+        constructor(options: VisualConstructorOptions) {
+        }
+
+        public update(options: VisualUpdateOptions) {
+
+        }
+
+        private static parseSettings(dataView: DataView): VisualSettings {
+            return VisualSettings.parse(dataView) as VisualSettings;
+        }
+
+        /** 
+         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
+         * objects and properties you want to expose to the users in the property pane.
+         * 
+         */
+        public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+            return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+        }
+    }
+}
