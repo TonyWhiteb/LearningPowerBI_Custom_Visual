1bf8ecd6ad871f6bd7bd26e683718dc1f4e8c1e4
TonyWhite <tonybh2016@gmail.com>
2017-07-24 16:10:18 -0500

    'single_complete'

--- nameCard/src/visual.ts
+++ nameCard/src/visual.ts
@@ -1,59 +1,68 @@
-/*
- *  Power BI Visual CLI
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
 
 module powerbi.extensibility.visual {
-    "use strict";
+
     export class Visual implements IVisual {
-        private target: HTMLElement;
-        private updateCount: number;
-        private settings: VisualSettings;
+        private host: IVisualHost; 
+        private svg: d3.Selection<SVGElement>; 
+        private container: d3.Selection<SVGElement>; 
+
+        private textValue: d3.Selection<SVGElement>; 
+        private textLabel: d3.Selection<SVGElement>; 
+
 
         constructor(options: VisualConstructorOptions) {
-            console.log('Visual constructor', options);
-            this.target = options.element;
-            this.updateCount = 0;
+            this.svg = d3.select(options.element)  
+                         .append('svg')  
+                         .classed('circleCard', true); 
+            this.container = this.svg.append("g")  
+                                     .classed('container', true); 
+
+            this.textValue = this.container.append("text")  
+                                           .classed("textValue", true); 
+            this.textLabel = this.container.append("text")  
+                                           .classed("textLabel", true);
+
         }
 
         public update(options: VisualUpdateOptions) {
-            this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-            console.log('Visual update', options);
-            this.target.innerHTML = `<p>Update count: <em>${(this.updateCount++)}</em></p>`;
+            let dataView: DataView = options.dataViews[0];
+            let width: number = options.viewport.width; 
+            let height: number = options.viewport.height; 
+            
+            this.svg.attr({  
+                width: width,  
+                height: height 
+            }); 
+
+            
+                
+            let fontSizeValue: number = Math.min(width, height) / 2; 
+            
+            this.textValue  
+                .text(dataView.single.value as number)  
+                .attr({   
+                    x: "50%", 
+                    y: "50%",   
+                    dy: "0.35em",   
+                    "text-anchor": "middle"  
+                }).style("font-size", fontSizeValue + "px"); 
+                
+            let fontSizeLabel: number = fontSizeValue / 4; 
+            
+            this.textLabel  
+                .text(dataView.metadata.columns[0].displayName)  
+                .attr({   
+                    x: "50%",   
+                    y: height / 2,   
+                    dy: fontSizeValue / 1.2,   
+                    "text-anchor": "middle"  
+                }).style("font-size", fontSizeLabel + "px")
+
         }
 
         private static parseSettings(dataView: DataView): VisualSettings {
             return VisualSettings.parse(dataView) as VisualSettings;
         }
 
-        /** 
-         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
-         * objects and properties you want to expose to the users in the property pane.
-         * 
-         */
-        public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-            return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
-        }
     }
 }
